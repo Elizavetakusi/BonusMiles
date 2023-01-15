7ef73ed6fab63a356962707235e6f873089d3b42
Liza Stepanova <tuchafeb@mail.ru>
2023-01-15 22:05:19 +0300

    Initial commit

---
+++ src/bonusMile.java
@@ -0,0 +1,9 @@
+public class bonusMile {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int bonusPrice = 20;
+        int bonusAmount = ticketPrice / bonusPrice;
+        System.out.println((bonusAmount) + " количество бонусных миль, начисленных за купленный билет ");
+    }
+
+    }
